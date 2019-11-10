7d885033fb4a92560bf0be720c89a9e348851c60
TebaiOsama <56879757+TebaiOsama@users.noreply.github.com>
2019-11-11 00:10:53 +0100

    Update and rename hotel.cpp to Hotel.cpp

--- Hotel.cpp
+++ Hotel.cpp
@@ -1,9 +1,9 @@
 /** 
-  * File:     hotel.cpp 
-  * Author:   O. Tebai (Osama_Tebai@etu.u-bourgogne.fr)
+  * File:     Hotel.cpp 
+  * Author:   O.Tebai (Osama_Tebai@etu.u-bourgogne.fr)
   * Date:     Fall 2019 
   * Course:   C-C++ Programming / Esirem 3A Informatique Electronique 
-  * Summary:  Definition of the class Date
+  * Summary:  Definition of the class Hotel
   */
 
 #include <string>
